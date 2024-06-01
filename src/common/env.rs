e7de2935509315879172f2c9f7c3d586b1d729b5
Alexander Harvey <harvey.alex01@gmail.com>
2024-06-01 14:06:59 +1000

    some main stuff

---
+++ src/common/env.rs
@@ -0,0 +1,1 @@
+use revm::primatives::actors
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
+use std::sync::Arc;
+
 use anyhow::Result;
+use dotenv::dotenv;
+use dotenv::var;
+use ethers::providers::{Provider, Ws};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv()?;
     println!("Hello, world!");
-    // to build a arbitrage bot need to do the follwing:
+
+    let ws_url = var::<&str>("WS_URL").unwrap();
+
+    let ws_client = Ws::connect(ws_url).await?;
+
+    let provider = Arc::new(Provider::new(ws_client));
+
+    // to build a arbitrage bot need to do the following:
     // 1. create an arb bot contract that uses other contract but inserts it's own swap function
     // 2. create code that scans for price changes, and simulates transactions
     //      - to test this we could start off by creating a simulation of usdt / usdc swaps
     // 3.
     // 4.
+
     Ok(())
 }
