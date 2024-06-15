82b81da89b6175a925148fbb1101ff3db631ef7d
Alexander Harvey <harvey.alex01@gmail.com>
2024-06-15 19:25:07 +1000

    works

--- src/arbitrage/simulation.rs
+++ src/arbitrage/simulation.rs
@@ -1,91 +1,18 @@
-use crate::common::abi::{decode_get_amount_out_response, get_amount_out_calldata};
-use crate::common::helpers::*;
-use alloy::providers::ProviderBuilder;
-use alloy::sol;
+use crate::common::revm::{EvmSimulator, Tx};
+use alloy::contract::{ContractInstance, Interface};
+use alloy::dyn_abi::DynSolValue;
+use alloy::eips::BlockId;
+use alloy::providers::{Provider, ProviderBuilder, RootProvider};
+use alloy::pubsub::PubSubFrontend;
+use alloy::rpc::client::WsConnect;
+use alloy::signers::wallet::LocalWallet;
+
+use alloy_json_abi::JsonAbi;
 use anyhow::Result;
-use dotenv::var;
-use revm::db::{AlloyDB, CacheDB};
-use revm::primitives::{address, Address, Bytecode, Bytes, U256};
+use revm::primitives::{address, AccountInfo, Address, Bytecode, FixedBytes, U256};
+use revm::DummyStateDB;
 use std::str::FromStr;
-use std::sync::Arc;
-use url::Url;
-use FlashloanArbitrage::executeArbitrageCall;
 
-sol! {
-    interface IUniswapV2Pair {
-        function token0() external view returns (address);
-        function token1() external view returns (address);
-        function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
-    }
-
-    interface IUniswapV3Pool {
-        function slot0() external view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, uint8 feeProtocol, bool unlocked);
-        function swap(address recipient, bool zeroForOne, int256 amountSpecified, uint160 sqrtPriceLimitX96, bytes calldata data) external returns (int256 amount0, int256 amount1);
-    }
-
-    interface IERC20 {
-        function balanceOf(address owner) external view returns (uint);
-        function approve(address spender, uint amount) external returns (bool);
-        function transfer(address recipient, uint amount) external returns (bool);
-    }
-}
-
-sol! {
-
-contract FlashloanArbitrage {
-    address public pool1;
-    address public pool2;
-    address public weth;
-    address public usdc;
-
-    constructor(address _pool1, address _pool2, address _weth, address _usdc) {
-        pool1 = _pool1;
-        pool2 = _pool2;
-        weth = _weth;
-        usdc = _usdc;
-    }
-
-    function executeArbitrage(uint amount) external {
-        // Call the flashloan function with the encoded data
-        IUniswapV3Pool(pool1).swap(
-            address(this),
-            true,
-            int256(amount),
-            0,
-            abi.encode(amount)
-        );
-    }
-
-    function uniswapV3Callback(int256 amount0Delta, int256 amount1Delta, bytes calldata data) external {
-        require(msg.sender == pool1 || msg.sender == pool2, "Invalid sender");
-
-        uint amount = abi.decode(data, (uint));
-
-        if (msg.sender == pool1) {
-            // Swap WETH for USDC on pool1
-            IUniswapV3Pool(pool1).swap(
-                address(this),
-                true,
-                int256(amount),
-                0,
-                abi.encode(amount)
-            );
-        } else {
-            // Swap USDC for WETH on pool2
-            IUniswapV3Pool(pool2).swap(
-                address(this),
-                false,
-                int256(amount),
-                0,
-                abi.encode(amount)
-            );
-
-            // Repay the flashloan
-            IERC20(weth).transfer(pool1, amount + (amount / 1000)); // Include a small fee
-        }
-    }
-}
-}
 pub async fn simulation(target_pool: Address, adjacent_pool: Address) -> Result<()> {
     //      - Simulation:
     //          - Simply we are going to simulate:
@@ -94,139 +21,74 @@ pub async fn simulation(target_pool: Address, adjacent_pool: Address) -> Result<
     //          - using weth gained, by required amount on lower priced exchange
     //          - pay back loan with fee
     //          - revenue will be weth gained - weth used to repay loan
-    //          - gas cost is the cost of all 3 transactions
 
     // initialise cache and addresses
-    let http_url = var("HTTP_URL").expect("No HTTP_URL").to_string();
-    let http_url = Url::from_str(http_url.as_str()).unwrap();
-    let mut cache_db = init_cache(http_url).await?;
+    // let http_url = var("HTTP_URL").expect("No HTTP_URL").to_string();
+    // let http_url = Url::from_str(http_url.as_str()).unwrap();
+    let ws_client = WsConnect::new(std::env::var("WS_URL").expect("no ws url"));
+    let provider = ProviderBuilder::new().on_ws(ws_client).await?;
+    // let mut cache_db = init_cache(http_url).await?;
+    let mem_db = DummyStateDB::default();
 
-    // create transaction data
-    //
-    let volumes = volumes(U256::from(0), one_ether() / U256::from(10), 100);
+    let latest_block_number = provider.get_block_number().await?;
+    let block_id = BlockId::from_str(latest_block_number.to_string().as_str()).unwrap();
+    let latest_block = provider
+        .get_block(block_id, true)
+        .await?
+        .expect("Expected block");
+    let latest_gas_limit = latest_block.header.gas_limit;
+    let latest_gast_price = U256::from(latest_block.header.base_fee_per_gas.expect("gas"));
 
-    // borrow from pool 1
+    let wallet = LocalWallet::random();
+    let wallet_address = wallet.address().to_string();
+    let wallet_address = Address::from_str(wallet_address.as_str())?;
 
-    let borrow = Bytes::from(executeArbitrageCall {
-        amount: one_ether(),
-    });
-    for volume in volumes.into_iter() {
-        // Get call data from first pool
-        let calldata = get_amount_out_calldata(pool_500_addr(), weth_addr(), usdc_addr(), volume);
-        // Execute calldata on the test evm
-        let response = revm_revert(me(), custom_quoter_addr(), calldata, &mut cache_db)?;
-        // Get usdc amount out
-        let usdc_amount_out = decode_get_amount_out_response(response)?;
-        // Get calldata from second pool
-        let calldata = get_amount_out_calldata(
-            pool_3000_addr(),
-            usdc_addr(),
-            weth_addr(),
-            U256::from(usdc_amount_out),
-        );
-        // get weth amount out
-        let response = revm_revert(me(), custom_quoter_addr(), calldata, &mut cache_db)?;
-        let weth_amount_out = decode_get_amount_out_response(response)?;
+    let contract_info = AccountInfo::new(U256::ZERO, 0, FixedBytes::ZERO, arboo_bytecode());
+    let mut simulator = EvmSimulator::new_with_db(wallet_address, latest_block_number, mem_db);
+    simulator.deploy(wallet_address, contract_info.code.unwrap());
 
-        println!(
-            "{} WETH -> USDC {} -> WETH {}",
-            volume, usdc_amount_out, weth_amount_out
-        );
+    let abi = json_abi();
 
-        let weth_amount_out = U256::from(volume);
-        if weth_amount_out > volume {
-            let profit = weth_amount_out - volume;
-            println!("WETH profit: {}", profit);
-        } else {
-            println!("No profit.");
-        }
-    }
+    let abi = Interface::new(abi);
+    // make Contract from abi
+    let contract_address =
+        alloy::primitives::Address::from_str(simulator.owner.to_string().as_str()).unwrap();
+    let contract = ContractInstance::<Address, RootProvider<PubSubFrontend>, Interface>::new(
+        contract_address,
+        provider.clone(),
+        abi,
+    );
+    // set initial eth value;
+    let alloy_wallet_address = Address::from_str(wallet.address().to_string().as_str()).unwrap();
+    let initial_eth_balance = U256::from(100) * U256::from(10).pow(U256::from(18));
+    simulator.set_eth_balance(alloy_wallet_address, initial_eth_balance);
+
+    let value1: DynSolValue = U256::from(1000000000000000000 as i64).into();
+    let value2: DynSolValue = U256::ZERO.into();
+    let function_call = contract.encode_input("flash", &[value1, value2])?;
+
+    let function_call_data = revm::primitives::Bytes::copy_from_slice(&*function_call);
+
+    let new_tx = Tx {
+        caller: wallet_address,
+        transact_to: simulator.owner,
+        data: function_call_data,
+        value: U256::default(),
+        gas_limit: latest_gas_limit as u64,
+        gas_price: latest_gast_price,
+    };
+    // simulate that transaction working
+    let gas_used = match simulator.call(new_tx) {
+        Ok(result) => result.gas_used,
+        Err(_) => 0,
+    };
+
+    println!("Gas Used :{:?}", gas_used);
+
     Ok(())
 }
-
-async fn init_cache(http_url: url::Url) -> Result<AlloyCacheDB> {
-    let provider = Arc::from(ProviderBuilder::new().on_http(http_url));
-    let mut cache_db = CacheDB::new(AlloyDB::new(provider.clone(), Default::default()));
-
-    // init acount with official quoter address:
-    let mocked_balance = U256::MAX / U256::from(2);
-    insert_mapping_storage_slot(
-        weth_addr(),
-        U256::from(0),
-        pool_3000_addr(),
-        mocked_balance,
-        &mut cache_db,
-    )
-    .await?;
-    insert_mapping_storage_slot(
-        usdc_addr(),
-        U256::from(0),
-        pool_3000_addr(),
-        mocked_balance,
-        &mut cache_db,
-    )
-    .await?;
-    insert_mapping_storage_slot(
-        weth_addr(),
-        U256::from(0),
-        pool_500_addr(),
-        mocked_balance,
-        &mut cache_db,
-    )
-    .await?;
-    insert_mapping_storage_slot(
-        usdc_addr(),
-        U256::from(0),
-        pool_500_addr(),
-        mocked_balance,
-        &mut cache_db,
-    )
-    .await?;
-
-    init_account(pool_500_addr(), &mut cache_db, provider.clone()).await?;
-
-    let mocked_erc20 = include_str!("../bytecode/generic_erc20.hex");
-    let mocked_erc20 = Bytes::from_str(mocked_erc20).unwrap();
-    let mocked_erc20 = Bytecode::new_raw(mocked_erc20);
-
-    init_account_with_bytecode(weth_addr(), mocked_erc20.clone(), &mut cache_db).await?;
-    init_account_with_bytecode(usdc_addr(), mocked_erc20.clone(), &mut cache_db).await?;
-
-    let mocked_balance = U256::MAX / (U256::from(2));
-
-    insert_mapping_storage_slot(
-        weth_addr(),
-        U256::from(0),
-        pool_3000_addr(),
-        mocked_balance,
-        &mut cache_db,
-    )
-    .await?;
-    insert_mapping_storage_slot(
-        usdc_addr(),
-        U256::from(0),
-        pool_3000_addr(),
-        mocked_balance,
-        &mut cache_db,
-    )
-    .await?;
-    insert_mapping_storage_slot(
-        weth_addr(),
-        U256::from(0),
-        pool_500_addr(),
-        mocked_balance,
-        &mut cache_db,
-    )
-    .await?;
-    insert_mapping_storage_slot(
-        usdc_addr(),
-        U256::from(0),
-        pool_500_addr(),
-        mocked_balance,
-        &mut cache_db,
-    )
-    .await?;
-    Ok(cache_db)
+pub fn one_ether() -> U256 {
+    "1000000000000000000".parse().unwrap()
 }
 
 pub fn me() -> Address {
@@ -257,4 +119,111 @@ pub fn pool_500_addr() -> Address {
 // WETH/USDC fee 3000
 pub fn pool_3000_addr() -> Address {
     address!("8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8")
+}
+
+pub fn arboo_bytecode() -> Bytecode {
+    let bytes = "0x60e060405234801561001057600080fd5b506040516109ba3803806109ba83398101604081905261002f9161013e565b6001600160a01b038116608081905260408051630dfe168160e01b81529051630dfe1681916004808201926020929091908290030181865afa158015610079573d6000803e3d6000fd5b505050506040513d601f19601f8201168201806040525081019061009d919061013e565b6001600160a01b031660a0816001600160a01b0316815250506080516001600160a01b031663d21220a76040518163ffffffff1660e01b8152600401602060405180830381865afa1580156100f6573d6000803e3d6000fd5b505050506040513d601f19601f8201168201806040525081019061011a919061013e565b6001600160a01b031660c05250600080546001600160a01b0319163317905561016e565b60006020828403121561015057600080fd5b81516001600160a01b038116811461016757600080fd5b9392505050565b60805160a05160c0516107fa6101c06000396000818161038b01526104db0152600081816102e301526104040152600081816101160152818161023e01528181610433015261050a01526107fa6000f3fe608060405234801561001057600080fd5b506004361061004c5760003560e01c8063828cc5ce146100515780638da5cb5b14610066578063b29a814014610095578063e9cbafb0146100a8575b600080fd5b61006461005f3660046105b1565b6100bb565b005b600054610079906001600160a01b031681565b6040516001600160a01b03909116815260200160405180910390f35b6100646100a33660046105ef565b610188565b6100646100b6366004610619565b610233565b60408051606080820183528482526020808301858152339385019384528451918201879052518185015291516001600160a01b03908116838301528351808403909201825260808301938490526312439b2f60e21b909352917f0000000000000000000000000000000000000000000000000000000000000000169063490e6cbc90610151903090879087908790608401610699565b600060405180830381600087803b15801561016b57600080fd5b505af115801561017f573d6000803e3d6000fd5b50505050505050565b6000546001600160a01b031633146101d35760405162461bcd60e51b81526020600482015260096024820152682727aa2fa7aba722a960b91b60448201526064015b60405180910390fd5b811580156101e8576001811461021b57505050565b60405163a9059cbb60e01b81523360048201528260248201526000806044836000885af161021557600080fd5b50505050565b60008060008085335af161022e57600080fd5b505050565b336001600160a01b037f0000000000000000000000000000000000000000000000000000000000000000161461029c5760405162461bcd60e51b815260206004820152600e60248201526d1b9bdd08185d5d1a1bdc9a5e995960921b60448201526064016101ca565b60006102aa82840184610706565b905084156103545760408181015190516323b872dd60e01b81526001600160a01b039182166004820152306024820152604481018790527f0000000000000000000000000000000000000000000000000000000000000000909116906323b872dd906064016020604051808303816000875af115801561032e573d6000803e3d6000fd5b505050506040513d601f19601f820116820180604052508101906103529190610774565b505b83156103fc5760408181015190516323b872dd60e01b81526001600160a01b039182166004820152306024820152604481018690527f0000000000000000000000000000000000000000000000000000000000000000909116906323b872dd906064016020604051808303816000875af11580156103d6573d6000803e3d6000fd5b505050506040513d601f19601f820116820180604052508101906103fa9190610774565b505b84156104d3577f00000000000000000000000000000000000000000000000000000000000000006001600160a01b031663a9059cbb7f0000000000000000000000000000000000000000000000000000000000000000878460000151610462919061079d565b6040516001600160e01b031960e085901b1681526001600160a01b03909216600483015260248201526044016020604051808303816000875af11580156104ad573d6000803e3d6000fd5b505050506040513d601f19601f820116820180604052508101906104d19190610774565b505b83156105aa577f00000000000000000000000000000000000000000000000000000000000000006001600160a01b031663a9059cbb7f0000000000000000000000000000000000000000000000000000000000000000868460200151610539919061079d565b6040516001600160e01b031960e085901b1681526001600160a01b03909216600483015260248201526044016020604051808303816000875af1158015610584573d6000803e3d6000fd5b505050506040513d601f19601f820116820180604052508101906105a89190610774565b505b5050505050565b600080604083850312156105c457600080fd5b50508035926020909101359150565b80356001600160a01b03811681146105ea57600080fd5b919050565b6000806040838503121561060257600080fd5b61060b836105d3565b946020939093013593505050565b6000806000806060858703121561062f57600080fd5b8435935060208501359250604085013567ffffffffffffffff8082111561065557600080fd5b818701915087601f83011261066957600080fd5b81358181111561067857600080fd5b88602082850101111561068a57600080fd5b95989497505060200194505050565b60018060a01b03851681526000602085602084015284604084015260806060840152835180608085015260005b818110156106e25785810183015185820160a0015282016106c6565b50600060a0828601015260a0601f19601f8301168501019250505095945050505050565b60006060828403121561071857600080fd5b6040516060810181811067ffffffffffffffff8211171561074957634e487b7160e01b600052604160045260246000fd5b80604052508235815260208301356020820152610768604084016105d3565b60408201529392505050565b60006020828403121561078657600080fd5b8151801515811461079657600080fd5b9392505050565b808201808211156107be57634e487b7160e01b600052601160045260246000fd5b9291505056fea26469706673582212201f11bf35a265fe6cf7a7a681e59372eebee6756b5c0d129c7a844608b62a108564736f6c63430008180033".parse().unwrap();
+    return Bytecode::new_raw(bytes);
+}
+
+pub fn json_abi() -> JsonAbi {
+    let abi: JsonAbi = serde_json::from_str(
+        r#"
+        [
+        {
+           "type":"constructor",
+           "inputs":[
+              {
+                 "name":"_pool",
+                 "type":"address",
+                 "internalType":"address"
+              }
+           ],
+           "stateMutability":"nonpayable"
+        },
+        {
+           "type":"function",
+           "name":"flash",
+           "inputs":[
+              {
+                 "name":"amount0",
+                 "type":"uint256",
+                 "internalType":"uint256"
+              },
+              {
+                 "name":"amount1",
+                 "type":"uint256",
+                 "internalType":"uint256"
+              }
+           ],
+           "outputs":[
+
+           ],
+           "stateMutability":"nonpayable"
+        },
+        {
+           "type":"function",
+           "name":"owner",
+           "inputs":[
+
+           ],
+           "outputs":[
+              {
+                 "name":"",
+                 "type":"address",
+                 "internalType":"address"
+              }
+           ],
+           "stateMutability":"view"
+        },
+        {
+           "type":"function",
+           "name":"recoverToken",
+           "inputs":[
+              {
+                 "name":"token",
+                 "type":"address",
+                 "internalType":"address"
+              },
+              {
+                 "name":"amount",
+                 "type":"uint256",
+                 "internalType":"uint256"
+              }
+           ],
+           "outputs":[
+
+           ],
+           "stateMutability":"nonpayable"
+        },
+        {
+           "type":"function",
+           "name":"uniswapV3FlashCallback",
+           "inputs":[
+              {
+                 "name":"fee0",
+                 "type":"uint256",
+                 "internalType":"uint256"
+              },
+              {
+                 "name":"fee1",
+                 "type":"uint256",
+                 "internalType":"uint256"
+              },
+              {
+                 "name":"data",
+                 "type":"bytes",
+                 "internalType":"bytes"
+              }
+           ],
+           "outputs":[
+
+           ],
+           "stateMutability":"nonpayable"
+        }
+        ]
+        "#,
+    )
+    .unwrap();
+    abi
 }
--- src/common/revm.rs
+++ src/common/revm.rs
@@ -1,1 +1,193 @@
+use anyhow::{anyhow, Result};
+use revm::db::EmptyDBTyped;
+use revm::primitives::{Address, Bytes, Log};
 
+use revm::{
+    db::CacheDB,
+    primitives::{
+        keccak256, AccountInfo, Bytecode, ExecutionResult, Output, TransactTo, B256, U256,
+    },
+    EVM,
+};
+use std::convert::Infallible;
+
+#[derive(Debug, Clone, Default)]
+pub struct VictimTx {
+    pub tx_hash: B256,
+    pub from: Address,
+    pub to: Address,
+    pub data: Bytes,
+    pub value: U256,
+    pub gas_price: U256,
+    pub gas_limit: Option<u64>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Tx {
+    pub caller: Address,
+    pub transact_to: Address,
+    pub data: Bytes,
+    pub value: U256,
+    pub gas_price: U256,
+    pub gas_limit: u64,
+}
+
+impl Tx {
+    pub fn from(tx: VictimTx) -> Self {
+        let gas_limit = match tx.gas_limit {
+            Some(gas_limit) => gas_limit,
+            None => 5000000,
+        };
+        Self {
+            caller: tx.from,
+            transact_to: tx.to,
+            data: tx.data,
+            value: tx.value,
+            gas_price: tx.gas_price,
+            gas_limit,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TxResult {
+    pub output: Bytes,
+    pub logs: Option<Vec<Log>>,
+    pub gas_used: u64,
+    pub gas_refunded: u64,
+}
+
+#[derive(Clone)]
+pub struct EvmSimulator {
+    pub owner: Address,
+    pub evm: EVM<CacheDB<EmptyDBTyped<Infallible>>>,
+    pub block_number: u64,
+}
+
+impl EvmSimulator {
+    pub fn new_with_db(
+        owner: Address,
+        block_number: u64,
+        db: CacheDB<EmptyDBTyped<Infallible>>,
+    ) -> Self {
+        let mut evm = EVM::new();
+        evm.database(db);
+
+        evm.env.block.number = U256::from(block_number + 1);
+        evm.env.block.coinbase = "0xDAFEA492D9c6733ae3d56b7Ed1ADB60692c98Bc5"
+            .parse()
+            .unwrap();
+
+        Self {
+            owner,
+            evm,
+            block_number,
+        }
+    }
+
+    pub fn insert_db(&mut self, db: CacheDB<EmptyDBTyped<Infallible>>) {
+        let mut evm = EVM::new();
+        evm.database(db);
+
+        self.evm = evm;
+    }
+
+    pub fn get_block_number(&mut self) -> U256 {
+        self.evm.env.block.number.into()
+    }
+
+    pub fn get_coinbase(&mut self) -> Address {
+        self.evm.env.block.coinbase.into()
+    }
+
+    pub fn get_base_fee(&mut self) -> U256 {
+        self.evm.env.block.basefee.into()
+    }
+
+    pub fn set_base_fee(&mut self, base_fee: U256) {
+        self.evm.env.block.basefee = base_fee.into();
+    }
+
+    pub fn staticcall(&mut self, tx: Tx) -> Result<TxResult> {
+        self._call(tx, false)
+    }
+
+    pub fn call(&mut self, tx: Tx) -> Result<TxResult> {
+        self._call(tx, true)
+    }
+
+    pub fn _call(&mut self, tx: Tx, commit: bool) -> Result<TxResult> {
+        self.evm.env.tx.caller = tx.caller.into();
+        self.evm.env.tx.transact_to = TransactTo::Call(tx.transact_to.into());
+        self.evm.env.tx.data = tx.data;
+        self.evm.env.tx.value = tx.value.into();
+        self.evm.env.tx.gas_price = tx.gas_price.into();
+        self.evm.env.tx.gas_limit = tx.gas_limit;
+
+        let result;
+
+        if commit {
+            result = match self.evm.transact_commit() {
+                Ok(result) => result,
+                Err(e) => return Err(anyhow!("EVM call failed: {:?}", e)),
+            };
+        } else {
+            let ref_tx = self
+                .evm
+                .transact_ref()
+                .map_err(|e| anyhow!("EVM staticcall failed: {:?}", e))?;
+            result = ref_tx.result;
+        }
+
+        let output = match result {
+            ExecutionResult::Success {
+                gas_used,
+                gas_refunded,
+                output,
+                logs,
+                ..
+            } => match output {
+                Output::Call(o) => TxResult {
+                    output: o,
+                    logs: Some(logs),
+                    gas_used,
+                    gas_refunded,
+                },
+                Output::Create(o, _) => TxResult {
+                    output: o,
+                    logs: Some(logs),
+                    gas_used,
+                    gas_refunded,
+                },
+            },
+            ExecutionResult::Revert { gas_used, output } => {
+                return Err(anyhow!(
+                    "EVM REVERT: {:?} / Gas used: {:?}",
+                    output,
+                    gas_used
+                ))
+            }
+            ExecutionResult::Halt { reason, .. } => return Err(anyhow!("EVM HALT: {:?}", reason)),
+        };
+        println!("Output: {:?}", output);
+        Ok(output)
+    }
+
+    pub fn insert_account_info(&mut self, target: Address, account_info: AccountInfo) {
+        self.evm
+            .db
+            .as_mut()
+            .unwrap()
+            .insert_account_info(target, account_info);
+    }
+
+    pub fn deploy(&mut self, target: Address, bytecode: Bytecode) {
+        let contract_info = AccountInfo::new(U256::ZERO, 0, B256::ZERO, bytecode);
+        self.insert_account_info(target, contract_info);
+    }
+    pub fn set_eth_balance(&mut self, target: Address, amount: U256) {
+        let user_balance = amount.into();
+        let user_info = AccountInfo::new(user_balance, 0, B256::ZERO, Bytecode::default());
+        self.insert_account_info(target.into(), user_info);
+    }
+}
