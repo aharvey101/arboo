4f7da94333d9237c5729126b6cd79ff36be07177
Alexander Harvey <harvey.alex01@gmail.com>
2024-06-22 23:01:07 +1000

    working v3-v2

--- contracts/src/arboo_aave_uv3.sol
+++ contracts/src/arboo_aave_uv3.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+/*// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -95,4 +95,4 @@ contract FlashloanArbitrage is IFlashLoanReceiver {
     ) external pure returns (bytes4) {
         return this.onERC20Received.selector;
     }
-}
+}*/
--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,5 +1,2 @@
-pub mod abi;
-pub mod env;
-pub mod helpers;
 pub mod pools;
 pub mod revm;
